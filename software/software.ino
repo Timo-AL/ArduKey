5c246845bb8f0a41a46bfaf78f436dbbce2b1190
Timo-AL <smcaijian@live.com>
2019-05-20 11:44:01 +0800

    Update software.ino

--- software/software.ino
+++ software/software.ino
@@ -6,8 +6,8 @@
 #define DT 1
 #define SW 2//旋转编码器定义
 
-#define MK1 0
-#define MK2 1//开关按键定义
+#define MK1 18
+#define MK2 19//开关按键定义
 
 #define PS  0
 #define PR  1
@@ -27,13 +27,13 @@
 #define X5 15//横行定义，读取时低电平
 int mod;//键盘模式变量
 
-bool key[5][5]={0}; //存储键盘当前状态用的数组
-bool key_[5][5]={0};//存储键盘上次状态用的数组
+bool key[5][5]={0};   //存储键盘当前状态用的数组
+bool key_[5][5]={0};  //存储键盘上次状态用的数组
 
 void wheel(bool direct)  //用于模拟鼠标滚轮的函数
 {
   static uint8_t m[4]={0,0,0,0};
-  short dir=1;   //修改这个值可以改变速度，然而1以及很快了
+  short dir=1;   //修改这个值可以改变速度，然而1已经很快了
   if(direct) dir*=-1;
   m[3] = dir;
   HID().SendReport(1,m,4);
@@ -59,14 +59,17 @@ void setup()   //不就是初始化吗
   pinMode(Y2,INPUT_PULLUP);
   pinMode(Y3,INPUT_PULLUP);
   pinMode(Y4,INPUT_PULLUP);
-  pinMode(Y5,INPUT_PULLUP);//初始化矩阵键盘状态
+  pinMode(Y5,INPUT_PULLUP);
+  high();                   //初始化矩阵键盘状态
+
+  pinMode(MK1,INPUT_PULLUP);
+  pinMode(MK2,INPUT_PULLUP);//初始化模式开关
   
   pinMode(SW,INPUT);
   digitalWrite(SW,HIGH);
   pinMode(CLK,INPUT);
   pinMode(DT,INPUT);
   attachInterrupt(2,ClockChanged,CHANGE);//旋转编码器初始化,中断函数ClockChanged
-  high();
   Serial.begin(9600);
 }
 
@@ -106,6 +109,7 @@ void scan()   //我来扫描键盘
     key[4][2]=digitalRead(Y3);
     key[4][3]=digitalRead(Y4);
     key[4][4]=digitalRead(Y5);
+    high();
 }
 
 void ClockChanged()  //旋转编码器处理函数
@@ -128,21 +132,37 @@ void ClockChanged()  //旋转编码器处理函数
       tra=false;
   }
   sw=digitalRead(SW);
-  wheel(!tra);
+  if(sw)
+    wheel(!tra);
+  else
+  {
+    if(tra)
+      {
+        Keyboard.press(KEY_LEFT_ARROW);
+        delayMicroseconds(50);
+        Keyboard.release(KEY_LEFT_ARROW);
+      }
+    else
+    {
+       Keyboard.press(KEY_RIGHT_ARROW);
+       delayMicroseconds(50);
+       Keyboard.release(KEY_RIGHT_ARROW);
+    }
+  }
 }
 
 void mode() //获取键盘模式
 {
   int mk1,mk2;
-  mk1=analogRead(MK1);
-  mk2=analogRead(MK2);
+  mk1=digitalRead(MK1);
+  mk2=digitalRead(MK2);
   if(  (mk1) &&  (mk2) ) mod=PS;
   if( !(mk1) &&  (mk2) ) mod=PR;
   if(  (mk1) && !(mk2) ) mod=OFF;
   if( !(mk1) && !(mk2) ) mod=SAI;
 }
 
-void runnaX1()
+void runnaX1()  //key1～key5功能设置
 {
   if(key[0][0] && key[0][0]!=key_[0][0])
   {
@@ -162,42 +182,457 @@ void runnaX1()
        break;
    }
   }
-  ;
+  if(key[0][1] && key[0][1]!=key_[0][1])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[0][2] && key[0][2]!=key_[0][2])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[0][3] && key[0][3]!=key_[0][3])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[0][4] && key[0][4]!=key_[0][4])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
 }
-void runnaX2()
+void runnaX2()  //key6～key10功能设置
 {
-  ;
+  if(key[1][0] && key[1][0]!=key_[1][0])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[1][1] && key[1][1]!=key_[1][1])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[1][2] && key[1][2]!=key_[1][2])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[1][3] && key[1][3]!=key_[1][3])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[1][4] && key[1][4]!=key_[1][4])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
 }
-void runnaX3()
+void runnaX3()  //key11～key15功能设置
 {
-  ;
+  if(key[2][0] && key[2][0]!=key_[2][0])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[2][1] && key[2][1]!=key_[2][1])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[2][2] && key[2][2]!=key_[2][2])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[2][3] && key[2][3]!=key_[2][3])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[2][4] && key[2][4]!=key_[2][4])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
 }
-void runnaX4()
+void runnaX4()  //key16～key20功能设置
 {
-  ;
+  if(key[3][0] && key[3][0]!=key_[3][0])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[3][1] && key[3][1]!=key_[3][1])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[3][2] && key[3][2]!=key_[3][2])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[3][3] && key[3][3]!=key_[3][3])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[3][4] && key[3][4]!=key_[3][4])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
 }
-void runnaX5()
+void runnaX5()  //key21～key25功能设置
 {
-  ;
+  if(key[4][0] && key[4][0]!=key_[4][0])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[4][1] && key[4][1]!=key_[4][1])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[4][2] && key[4][2]!=key_[4][2])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[4][3] && key[4][3]!=key_[4][3])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
+  if(key[4][4] && key[4][4]!=key_[4][4])
+  {
+    switch(mod)
+    {
+     case PS:
+       Keyboard.print("a");
+       break;
+     case PR:
+       Keyboard.print("b");
+       break;
+     case OFF:
+       Keyboard.print("c");
+       break;
+     case SAI:
+       Keyboard.print("d");
+       break;
+   }
+  }
 }
 
 void loop()
 {
-  int check_row;
+  int check_row,shock_a,shock_b;
   mode();
   scan();
-  for(check_row=0;check_row<25;check_row++)
-  {
-    *(key[0]+check_row)=!*(key[0]+check_row);
-  }
-  for(check_row=0;check_row<25;check_row++)
-  {
-//   Serial.print(*(key[0]+check_row));
-//    Serial.print(*(key_[0]+check_row));
-//    Serial.print(" ");
-  }
-//  delay(1000);
-  Serial.println();
   runnaX1();
   runnaX2();
   runnaX3();
